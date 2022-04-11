547d7e46210e84d755e71dac2abef19949f5af39
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2022-04-11 18:55:37 +0900

    코로나 확진 자 수 추가

--- asdf.py
+++ asdf.py
@@ -2,6 +2,8 @@ import discord, asyncio, random, os
 from discord.ext.commands import Bot
 from bs4 import BeautifulSoup
 import requests
+import time
+import datetime
 
 intents = discord.Intents.default()
 bot = Bot(command_prefix='!', intents=intents)
@@ -29,26 +31,26 @@ covid19_data = requests.get("https://m.news.naver.com/covid19/index")
 
 soup2 = BeautifulSoup(covid19_data.text, 'lxml')
 
-Onecha = soup2.find("dt", 'term')
+onecha = soup2.find_all("strong", 'value increase _count _require_set_with')
 
-Onecha = Onecha.find("div", 'new')
+# school_data = requests.get("https://sdh.sen.hs.kr/index.do")
 
-Onecha = Onecha.find_all("span")
+# soup1 = BeautifulSoup(school_data.text, 'lxml')
+
+# lunch = soup1.find("div", 'index_mlsv_box')
+
+# lunch = lunch.find("p", "text_contents")
+
+# lunch = lunch.find_all("a")
 
 #여기까지가 코로나 19  웹 크롤링
 
-
 # ========================================================================================================================
+
 
 @bot.event
 async def on_message(message):
-    
-    if message.content == "?코로나19 백신 현황":
-         embed = discord.Embed(title="코로나19 백신 현황",
-                              description=Onecha,
-                              color=0x3E6FDE)
-         embed.set_footer(text="오류가 있으시다면 문의 주세요 :) ")
-         await message.channel.send(embed=embed)
+
 
     if message.content == "?발로란트 상점":
         embed = discord.Embed(title="발로란트 상점 사이트",
@@ -58,37 +60,6 @@ async def on_message(message):
             url=
             "https://www.greened.kr/news/photo/202106/290289_309610_5828.jpg")
         embed.set_footer(text="발로란트 상점 우회하기")
-        await message.channel.send(embed=embed)
-
-    if message.content == "?시간표":
-        embed = discord.Embed(title="2-4 시간표",
-                              description="변경 필요할 시 문의 부탁드립니다..",
-                              color=0xfd5252)
-        embed.add_field(
-            name=":red_circle: 월요일",
-            value=str(
-                " **```1교시 영어 2교시 영어 3교시 음악 4교시 직업 5교시 직업 6교시 직업 7교시 직업```** "
-            ),
-            inline=False)
-        embed.add_field(
-            name=":orange_circle: 화요일",
-            value=
-            " **```1교시 탐색 2교시 국어 3교시 수학 4교시 체육 5교시 체육 6교시 음악 7교시 음악```** ",
-            inline=False)
-        embed.add_field(
-            name=":yellow_circle: 수요일",
-            value=" **```1교시 국어 2교시 탐색 3교시 탐색 4교시 창체 5교시 창체 6교시 창체 ```** ",
-            inline=False)
-        embed.add_field(
-            name=":green_circle: 목요일",
-            value=
-            " **```1교시 언어 2교시 언어 3교시 체육 4교시 논술 5교시 국어 6교시 수학 7교시 수학```** ",
-            inline=False)
-        embed.add_field(
-            name=":blue_circle: 금요일",
-            value=
-            " **```1교시 화면 2교시 화면 3교시 화면 4교시 탐색 5교시 논술 6교시 체육 7교시 영어```** ",
-            inline=False)
         await message.channel.send(embed=embed)
 
 
