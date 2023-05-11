f6248456958271dcf429e9f4a1455856756681dc
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-05-11 16:02:41 +0900

    last commit

--- apiProject.py
+++ apiProject.py
@@ -5,18 +5,26 @@ from urllib.parse import urlparse,parse_qs,urlencode
 url = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?serviceKey=DRr3Z3dZJqdLd1GnvsPNnStHUzR8%2Bh0eouQ%2Fkdw8IZJaS8cBCDYdPHNKPkFPTN%2F2Zrkr71sWx2WEP3nf3myURA%3D%3D&arsId=03567"
 print(parse_qs(urlparse(url).query))
 
+
+arsid = input()
 url2 = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?"
-params = {'serviceKey': ['DRr3Z3dZJqdLd1GnvsPNnStHUzR8+h0eouQ/kdw8IZJaS8cBCDYdPHNKPkFPTN/2Zrkr71sWx2WEP3nf3myURA=='],
-          'arsId': ['03567']}
+params = {'serviceKey': 'DRr3Z3dZJqdLd1GnvsPNnStHUzR8+h0eouQ/kdw8IZJaS8cBCDYdPHNKPkFPTN/2Zrkr71sWx2WEP3nf3myURA==',
+          'arsId': arsid }
 
 resURL = url2+urlencode(params)
-print(resURL )
-# response = requests.get(url)
-#
-# result = xmltodict.parse(response.text)
-# item = result['ServiceResult']['msgBody']['itemList']
-#
-# arrMsg1 = item['arrmsg1']
-# arrMsg2 = item['arrmsg2']
-# print(arrMsg1,'/',arrMsg2)
-
+print(resURL)
+response = requests.get(resURL)
+result = xmltodict.parse(response.text)
+items = result['ServiceResult']['msgBody']['itemList']
+if type(items) == list:
+    for item in items:
+        rtNM = item['rtNm']
+        arrMsg1 = item['arrmsg1']
+        arrMsg2 = item['arrmsg2']
+        print(rtNM, "/", arrMsg1, '/', arrMsg2)
+else:
+    item = result['ServiceResult']['msgBody']['itemList']
+    rtNM = item['rtNm']
+    arrMsg1 = item['arrmsg1']
+    arrMsg2 = item['arrmsg2']
+    print(rtNM, "/", arrMsg1, '/', arrMsg2)
