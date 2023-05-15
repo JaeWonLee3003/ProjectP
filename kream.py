f96e722274f331804af5daf8721ddd83da527e3a
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-05-15 15:53:34 +0900

    2023_05_15 last saved

--- kream.py
+++ kream.py
@@ -6,8 +6,10 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pymysql
+
 db = pymysql.connect(host='localhost', port=3306,user='root', password='manager')
-inserSQL = "INSERT INTO PyPj.KREAM_URL (URL, product_id) VALUES ( %s, %s )"
+insertSQL = "INSERT INTO PyPj.KREAM_URL (URL, product_id) VALUES ( %s, %s )"
+cursor = db.cursor()
 
 option = options.Options()
 option.add_experimental_option('detach', True)
@@ -15,27 +17,30 @@ service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service,
                           options=option)
 def getData():
-    driver.get("https://kream.co.kr/products/121041")
-    box = driver.find_element(By.CLASS_NAME,"main_title_box")
-    brand = box.find_element(By.CLASS_NAME,"brand").text
-    name = box.find_element(By.CLASS_NAME,"title").text
-    if name == '':
-        driver.refresh()
-    subname = box.find_element(By.CLASS_NAME,"sub_title").text
-    priceBox = driver.find_element(By.CLASS_NAME,"detail_price")
-    price = priceBox.find_element(By.CLASS_NAME,"num").text
-    #print(brand,name,subname,price)
-    detailBox = driver.find_elements(By.CLASS_NAME,'detail_box')
-    image = driver.find_element(By.CLASS_NAME,"item_inner")\
-    .find_element(By.TAG_NAME,"img").get_attribute("src")
-    print(image)
+     selectSQL = "SELECT URL, product_id, flag FROM Pypj.kream_url where flag = 'N';"
+     cursor.execute(selectSQL)
+     rows = cursor.fetchall()
+     for i in rows:
+         driver.get(i[0])
+         box = driver.find_element(By.CLASS_NAME,"main_title_box")
+         brand = box.find_element(By.CLASS_NAME,"brand").text
+         name = box.find_element(By.CLASS_NAME,"title").text
+         if name == '':
+             driver.refresh()
+             subname = box.find_element(By.CLASS_NAME,"sub_title").text
+             priceBox = driver.find_element(By.CLASS_NAME,"detail_price")
+             price = priceBox.find_element(By.CLASS_NAME,"num").text
+             detailBox = driver.find_elements(By.CLASS_NAME,'detail_box')\
+                 .find_element(By.TAG_NAME,"img").get_attribute("src")
+             image = driver.find_element(By.CLASS_NAME, "item_inner") \
+                 .find_element(By.TAG_NAME, "img").get_attribute("src")
+             print(image)
+             for detail in detailBox:
+                 title = detail.find_element(By.CLASS_NAME,"product_title")
+                 info = detail.find_element(By.CLASS_NAME,"product_info")
+                 print(title.text,info.text)
 
-    for detail in detailBox:
-        title = detail.find_element(By.CLASS_NAME,"product_title")
-        info = detail.find_element(By.CLASS_NAME,"product_info")
-        print(title.text,info.text)
-
-def main():
+def getURL():
     driver.get("https://kream.co.kr/search?tab=44&keyword=에어포스")
     time.sleep(3)
     total = driver.find_element(By.CLASS_NAME,
@@ -44,7 +49,7 @@ def main():
                 .replace(",","").strip())
     total = 500
     count = 0
-    while count == total:
+    while count < total:
         driver.execute_script("window.scrollTo"
                           "(0,document.body.scrollHeight)")
         time.sleep(1)
@@ -52,9 +57,15 @@ def main():
         print(len(itemList))
         count = len(itemList)
     for item in itemList:
-        href = item.find_element(By.TAG_NAME,"a")\
-            .get_attribute("href")
+        href = item.find_element(By.TAG_NAME,"a").get_attribute("href")
         print(href)
+        id = href.split("/")[4]
+        print(id)
+        cursor.execute(insertSQL,(href,id))
+        db.commit()
+
+def main():
+    getData()
 
 if __name__ == '__main__':
     main()
